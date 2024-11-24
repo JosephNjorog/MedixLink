e856ad70a9162950bad907f983a9961f3778f4cf
JosephNjorog <njojoe11173@gmail.com>
2024-11-25 01:24:22 +0300

    Uncomment routing for authentication and main layout in App component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-// import MainLayout from './components/layout/MainLayout';
-// import AuthLayout from './components/layout/AuthLayout';
-// import LoginPage from './pages/LoginPage';
-// import DashboardPage from './pages/DashboardPage';
+import MainLayout from './components/layout/MainLayout';
+import AuthLayout from './components/layout/AuthLayout';
+import LoginPage from './pages/LoginPage';
+import DashboardPage from './pages/DashboardPage';
 import PackageSelection from './components/Transactions/PackageSelection';
 
 function App() {
@@ -11,16 +11,16 @@ function App() {
     <Router>
       <Routes>
         {/* Auth routes */}
-        {/* <Route element={<AuthLayout />}>
-          <Route path="/login" element={<LoginPage />} /> */}
+        <Route element={<AuthLayout />}>
+          <Route path="/login" element={<LoginPage />} />
           <Route path="/packages" element={<PackageSelection />} />
         </Route>
         
-        {/* Main app routes
+        {/* Main app routes */}
         <Route element={<MainLayout />}>
-          <Route path="/" element={<DashboardPage />} /> */}
+          <Route path="/" element={<DashboardPage />} />
           {/* Other routes will be added as we develop features */}
-        {/* </Route> */}
+        </Route>
       </Routes>
     </Router>
   );
